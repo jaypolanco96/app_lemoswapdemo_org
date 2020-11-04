a07ee1e52cfbf944cfdc62b61d519e93d555318c
Lint Action <lint-action@samuelmeuli.com>
2020-11-04 16:45:48 +0000

    Fix code style issues with ESLint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-// clean factory and router on router:" 0x8dEC1BB14408C830069cDf40C5d3d08aD4663FaC" 
+// clean factory and router on router:" 0x8dEC1BB14408C830069cDf40C5d3d08aD4663FaC"
 //                           && factory: 0x383b3BF2B15EA2dE128148548AE68A99ee1186FF
 // its ready !
 export const ROUTER_ADDRESS = '0x8dEC1BB14408C830069cDf40C5d3d08aD4663FaC'
